abc7f274c64cf63c456bae8dcc839c72ec0c9c7a
Michael Sampietro <mssampietro@gmail.com>
2023-02-01 15:49:22 -0300

    test: Filter component

--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './style.module.css';
 
-type FilterProps = {
+export type FilterProps = {
   label: string;
   fieldName: string;
   applyFilter: (e: React.ChangeEvent<HTMLInputElement>, filterName: string) => void;
