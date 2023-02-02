7e284137480d64505d20768a41b4a06955925916
Michael Sampietro <mssampietro@gmail.com>
2023-02-01 23:48:21 -0300

    test: configure jest for pipeline

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: {
+    name: 'gc-operation-app',
+    color: 'magentaBright',
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    'src/*.{ts,tsx}',
+  ],
+};
